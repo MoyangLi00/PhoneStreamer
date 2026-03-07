72806655adbe569d2d561b6fa30d8f27e940391a
Moyang Li <limoyang2000@gmail.com>
2026-03-07 21:02:23 +0100

    Initial Commit

---
+++ PhoneStreamer/PhoneStreamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneStreamerApp.swift
+//  PhoneStreamer
+//
+//  Created by Moyang Li on 2026/3/7.
+//
+
+import SwiftUI
+
+@main
+struct PhoneStreamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
